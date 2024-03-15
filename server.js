21763c43388dc9473718578e7e587896300326b4
Rashed Kamal <rashedkamal423@gmail.com>
2024-03-15 19:12:52 -0400

    Editing server setup

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 import express from 'express';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 
-var app = express();
-app.use(express.static('public')); 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+const http = require('node:http');
+const bootstrap = require('bootstrap')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
 });
-app.get("/", (req, res) => {
- res.send('Hello World!')
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 app.get("/showfile", (req, res, next) => { 
- // show the page
-    const __filename = fileURLToPath(import.meta.url);
- const __dirname = path.dirname(__filename);
- const _retfile = path.join(__dirname, 'test.html');
-
- res.sendFile(_retfile);
-});
+    // show the page
+       const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const _retfile = path.join(__dirname, 'test.html');
+   
+    res.sendFile(_retfile);
+   });
