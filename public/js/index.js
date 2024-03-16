f393dfb7667d03008c97a1a1657c7038357f6e01
David-Parkerr <davidparker0040@gmail.com>
2024-03-15 22:49:23 -0400

    Added createAlbum function

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
 $(function(){
     
 });
+
+function createAlbum() {
+    const newAlbum = $("<div class='albumContainer'></div>");
+    const newImage = $("<div class='albumCoverItem'></div>");
+    $('.albumMosaicContainer').append(newAlbum).append(newImage);
+}
