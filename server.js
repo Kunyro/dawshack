d16722f8539c496cfc91ba54fe8a5380e9adf01d
Rashed Kamal <rashedkamal423@gmail.com>
2024-03-15 23:14:46 -0400

    Edited server.js to handle Python interactions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { PythonShell } from 'python-shell';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +14,23 @@ app.use(express.static(path.join(__dirname, 'html')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'html', 'index.html'));
+});
+
+app.post("/get_dominant_color", (req, res) => {
+  // extract the image URL from the request body
+  const imageUrl = req.body.imageUrl;
+
+  // execute the Python script using python-shell
+  PythonShell.run('./dominant_color/dominant_color.py', { args: [imageUrl] }, (err, results) => {
+    if (err) {
+      console.error('Error executing Python script:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const dominantColors = JSON.parse(results[0]);
+      // send the dominant colors back to the client
+      res.json({ dominantColors });
+    }
+  });
 });
 
 app.listen(port, hostname, () => {
