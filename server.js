9cdf56141b00bca6032f774770e30938d4886f33
David-Parkerr <davidparker0040@gmail.com>
2024-03-15 22:14:58 -0400

    Removed bootstrap and move html to public folder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'html')));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
 
 app.listen(port, hostname, () => {
