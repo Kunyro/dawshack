7a93cb423a46135c8e09ab751282b9773eb80697
Rashed Kamal <rashedkamal423@gmail.com>
2024-03-15 20:15:29 -0400

    Fixed server startup issues

--- server.js
+++ server.js
@@ -2,27 +2,19 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const http = require('node:http');
-const bootstrap = require('bootstrap')
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+app.use(express.static(path.join(__dirname, 'html')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'index.html'));
 });
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-app.get("/showfile", (req, res, next) => { 
-    // show the page
-       const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const _retfile = path.join(__dirname, 'test.html');
-   
-    res.sendFile(_retfile);
-   });
