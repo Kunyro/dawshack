8514ac26404f01fc74698f72e566482a4aa04646
David-Parkerr <davidparker0040@gmail.com>
2024-03-15 23:50:44 -0400

    Added js function that generates albums on html

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,25 @@
 $(function(){
-    
+    $.get('/data', function(data) {
+        const img = data.img;
+        const title = data.title;
+        const artist = data.artist;
+        createAlbum(img, title, artist);
+    });
 });
 
-function createAlbum() {
+export function createAlbum(img, title, artist) {
     const newAlbum = $("<div class='albumContainer'></div>");
-    const newImage = $("<div class='albumCoverItem'></div>");
-    $('.albumMosaicContainer').append(newAlbum).append(newImage);
+    const newImage = $("<img class='albumCoverItem'></img>");
+    newImage.attr('src', img);
+    const albumTitleItem = $("<h1 class='albumTitleItem'></h1>");
+    albumTitleItem.text(title);
+    const albumArtistItem = $("<h2 class='albumArtistItem'></h2>");
+    albumArtistItem.text(artist);
+    
+    // append the new elements to the album container
+    newAlbum.append(newImage);
+    newAlbum.append(albumTitleItem);
+    newAlbum.append(albumArtistItem);
+
+    $('.albumMosaicContainer').append(newAlbum);
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
+import { PythonShell } from 'python-shell';
 import { fileURLToPath } from 'url';
+import { createAlbum } from './public/js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -13,6 +15,21 @@ app.use(express.static(path.join(__dirname, 'html')));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
+});
+
+app.get("/data", (req, res) => {
+  PythonShell.run(path.join(__dirname, 'sample', 'path'), null, (err, results) => {
+    if (err) {
+      console.error('Error: ', err);
+      return;
+    }
+    const data = {
+      img: results[0],
+      title: results[1],
+      artist: results[2]
+    };
+    res.json(data);
+  });
 });
 
 app.listen(port, hostname, () => {
